11bc84d6fc66998a0f58c143ff9ce283b4ca4758
Md-Sefat-Alam <md.sefatalam@gmail.com>
2021-10-04 21:14:59 +0600

    not found page

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import notfoundImg from '../../images/404.jpg'
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            not found
+        <div className='notfound'>
+            <div>
+                <img src={notfoundImg} alt="" />
+                {/* <div>
+                    <h1>Page Not Found!!!</h1>
+                </div> */}
+                <button className="notFoundBtn">
+                    <Link to='/home'>
+                        Go to Home
+                    </Link>
+                </button>
+            </div>
         </div>
     );
 };
